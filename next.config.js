ff89c3e25aacbd8794d480005ac8539bfeb87a78
BélaCH <belam.chouaha@gmail.com>
2026-04-09 13:41:24 +0200

    fix: basePath pour GitHub Pages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  basePath: process.env.NODE_ENV === 'production' ? '/ifr-minima' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/ifr-minima/' : '',
 }
 
 module.exports = nextConfig
